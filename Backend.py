5b04aa19a58466be7911de84ffa7eb61d37890c0
Donald <59314046+DonaldCapodilupo@users.noreply.github.com>
2023-09-19 17:55:32 -0400

    Program moving between the Request and View windows correctly. Need to work on Backend and a little bit of Frontend.

--- Backend.py
+++ Backend.py
@@ -1,11 +1,11 @@
-import os, csv, datetime
+import os, csv, datetime, sqlite3
 
 today = str(datetime.date.today())
 
-def record_Request(form_dict):
 
+def record_Request(form_dict):
     list_of_values = list(form_dict.values())
-    list_of_values.extend(today)
+    list_of_values.insert(0, today)
 
     with open("Comp Time Ledger.csv", "a", newline="") as csv_file:
         csv_writer = csv.writer(csv_file)
@@ -13,13 +13,147 @@ def record_Request(form_dict):
 
 
 def program_Setup_On_Startup():
+    print("Checking to see if Comp Time Ledger.csv is present in current directory.")
     if "Comp Time Ledger.csv" not in os.listdir():
+        print("Comp Time Ledger.csv not found.")
+        print("Creating Comp Time Ledger.csv")
         with open("Comp Time Ledger.csv", "w", newline="") as csv_file:
             csv_writer = csv.writer(csv_file)
 
             csv_writer.writerow(
-                ["Date", "Employee", "Current Comp Time Balance", "Time and a Half Hours Worked", "Double Time Worked",
-                 "Compt Time Hours Requested", "Compt Time Hours Used", "Compt Time Available"])
+                ["Date", "Employee", "Current Comp Time Balance","Week Ending" "Time and a Half Hours Worked",
+                 "Double Time Worked","Compt Time Hours Requested", "Compt Time Hours Used", "Compt Time Available"])
             csv_file.close()
     else:
-        print("present.")
+        print("Comp Time Ledger.csv is present.")
+
+
+class Database_Modifier:
+    def __init__(self):
+        self.database_name = "Information.db"
+
+    def check_If_Table_Exists(self, table_name, list_of_columns):
+        db = sqlite3.connect(self.database_name)
+
+        list_of_columns = [column.replace(" ", "_").replace("-", "_") for column in list_of_columns]
+        print(list_of_columns)
+
+        print(table_name + " IS THE TABLE NAME")
+        print(
+            f"CREATE TABLE IF NOT EXISTS {table_name} (id INTEGER PRIMARY KEY AUTOINCREMENT  , {', '.join([f'{col} TEXT' for col in list_of_columns])})"
+        )
+
+        create_table_sql = f"CREATE TABLE IF NOT EXISTS {table_name} (id INTEGER PRIMARY KEY AUTOINCREMENT, {', '.join([f'{col} TEXT' for col in list_of_columns])})"
+
+        db.execute(create_table_sql)
+        db.commit()
+
+    def create_Database_Row(self, table_name, dict_of_values):
+        db = sqlite3.connect(self.database_name)
+
+        columns = [column_name.replace(" ", "_").replace("-", "_") for column_name in dict_of_values.keys()]
+        values = [value.replace(" ", "_").replace("-", "_") for value in dict_of_values.values()]
+
+        print("Values: ")
+        for i in values:
+            print(i)
+
+        insert_sql = f"INSERT INTO {table_name} ({', '.join(columns)}) VALUES ({', '.join(['?'] * len(columns))})"
+
+        db.execute(insert_sql, values)
+        db.commit()
+
+    def read_Database_Single_Table(self, table_name, case_number):
+        import sqlite3
+        import pandas as pd
+
+        sqliteConnection = sqlite3.connect(self.database_name)
+
+
+        df = pd.read_sql_query("SELECT * FROM " + table_name, sqliteConnection)
+
+
+
+        return_dict = {case_number:{table_name:{}}}
+
+        for index, row in df.iterrows():
+            if row.get(key="Case_Number") == case_number:
+                return_dict[case_number][table_name][
+                    str(row["Basic_Details_First_Name"] + " " + row["Basic_Details_Last_Name"])] = {}
+                for data_name, data_value in row.items():
+                    return_dict[case_number][table_name][
+                        str(row["Basic_Details_First_Name"] + " " + row["Basic_Details_Last_Name"])][
+                        str(data_name)] = str(data_value)
+
+        return return_dict
+
+    def read_Database_All_Tables(self, case_number):
+        import sqlite3
+        import pandas as pd
+
+        try:
+
+            # Making a connection between sqlite3
+            # database and Python Program
+            sqliteConnection = sqlite3.connect(self.database_name)
+
+            # If sqlite3 makes a connection with python
+            # program then it will print "Connected to SQLite"
+            # Otherwise it will show errors
+            print("Connected to SQLite")
+
+            # Getting all tables from sqlite_master
+            sql_query = """SELECT name FROM sqlite_master
+            WHERE type='table';"""
+
+            # Creating cursor object using connection object
+            cursor = sqliteConnection.cursor()
+
+            # executing our sql query
+            list_of_table_names = [table_name[0] for table_name in cursor.execute(sql_query).fetchall()]
+
+            return_dict = {case_number: {}}
+
+            for database_table in list_of_table_names:
+                df = pd.read_sql('SELECT * FROM ' + database_table, sqliteConnection)
+
+                if str(database_table) != "sqlite_sequence" :
+                    return_dict[case_number][database_table] = {}
+                print("Table Name: " + str(database_table))
+
+                for index, row in df.iterrows():
+                    print("Row : " + str(row.get(key="Case_Number")))
+                    if row.get(key="Case_Number") == case_number:
+
+                        return_dict[case_number][database_table][
+                            str(row["Basic_Details_First_Name"] + " " + row["Basic_Details_Last_Name"])] = {}
+                        for data_name, data_value in row.items():
+                            return_dict[case_number][database_table][str(row["Basic_Details_First_Name"] + " " + row["Basic_Details_Last_Name"])][str(data_name)] = str(data_value)
+
+        except AttributeError as error:
+            print("Failed to execute the above query", error)
+
+        finally:
+            # Inside Finally Block, If connection is
+            # open, we need to close it
+            if sqliteConnection:
+                # using close() method, we will close
+                # the connection
+                sqliteConnection.close()
+
+                # After closing connection object, we
+                # will print "the sqlite connection is
+                # closed"
+                print("the sqlite connection is closed")
+                return return_dict
+
+    def update_Database_Table(self):
+        pass
+
+    def destroy_Database_Row(self, table_name, id_number):
+        db = sqlite3.connect(self.database_name)
+
+        insert_sql = f"DELETE FROM {table_name} WHERE id = '{id_number}'"
+
+        db.execute(insert_sql)
+        db.commit()
--- app.py
+++ app.py
@@ -1,27 +1,77 @@
 from flask import Flask, redirect, request, render_template, url_for
-import json, csv
+import json, csv, sqlite3
 from Backend import program_Setup_On_Startup
-
+from markupsafe import escape
 app = Flask(__name__)
 
 
 @app.route('/', methods=["POST", "GET"])
+def main_Menu():
+    if request.method == "POST":
+        button_clicked = request.form['submit_button']
+        redirect_dict = {
+            "View Available Comp Time": "view_Comp_Time",
+            "Request Comp Time": "request_Comp_Time",
+
+        }
+
+        buttons_that_require_input = ["View Available Comp Time",]
+
+        employee = request.form["employee_name"]
+
+
+
+        if button_clicked in buttons_that_require_input:
+            return redirect(url_for(redirect_dict[button_clicked], employee=employee))
+        else:
+            return redirect(url_for(redirect_dict[button_clicked]))
+
+    else:
+        with open('static/Employee List.json') as json_file:
+            employee_dict = json.load(json_file)
+        return render_template("main.html", employees=employee_dict)
+
+
+
+
+@app.route('/View-Profile/<string:employee>', methods=["POST", "GET"])
+def view_Comp_Time(employee):
+    if request.method == "POST":
+        button_clicked = request.form['submit_button']
+        redirect_dict = {
+            "Request Comp Time": "request_Comp_Time",
+            "View Comp Time": "view_Comp_Time",
+
+        }
+
+        reports_that_require_case_numbers = ["Daily Activity Report", "Check Case Status", "Witness Statement",
+                                             "Person of Interest",
+                                             "Interview Report", "Create BOLO"]
+
+        if button_clicked in reports_that_require_case_numbers:
+            return redirect(url_for(redirect_dict[button_clicked],))
+        else:
+            return redirect(url_for(redirect_dict[button_clicked]))
+    else:
+        return render_template("View Comp Time.html", employee_name = escape(employee))
+
+
+@app.route('/Request-Comp-Time', methods=["POST", "GET"])
 def request_Comp_Time():
     if request.method == "POST":
-        from Backend import record_Request
-        form_data_dict = request.form
+        from Backend import Database_Modifier
+        data_dict = request.form.to_dict()
 
-        record_Request(form_data_dict)
-
-        print(request.form)
-        return render_template("View Comp Time.html")
+        Database_Modifier().check_If_Table_Exists("Comp_Time_Requests", data_dict.keys())
+        Database_Modifier().create_Database_Row("Comp_Time_Requests", data_dict)
+        return redirect(url_for("main_Menu", confirmation="Comp Time Requested Submitted"))
     else:
         with open('static/Employee List.json') as json_file:
             employee_dict = json.load(json_file)
         with open('static/Current Banked Hours.json') as json_file:
             current_banked_hours = json.load(json_file)
         starting_hour = list(current_banked_hours.values())[0]
-        return render_template("View Comp Time.html", employees=employee_dict, banked_hours=current_banked_hours,
+        return render_template("Request Comp Time.html", employees=employee_dict, banked_hours=current_banked_hours,
                                starting_hour=starting_hour)
 
 start = program_Setup_On_Startup()
